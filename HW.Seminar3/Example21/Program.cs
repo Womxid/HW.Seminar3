407e64b313205c7c1ea4ba5cde7936d4959f0990
Алексей Якунин <116656993+Womxid@users.noreply.github.com>
2022-11-01 18:59:08 +0300

    Add files via upload

---
+++ HW.Seminar3/Example21/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+Console.WriteLine("введите координаты двух точек в трех измерениях чтобы найти расстояние между ними. Начните с x1");
+double x1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Теперь введите координату x2");
+double x2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Теперь введите координату y1");
+double y1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Теперь введите координату y2");
+double y2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Теперь введите координату z1");
+double z1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Теперь введите координату z2");
+double z2 = double.Parse(Console.ReadLine());
+
+double distantion = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2));
+Console.WriteLine($"Расстояние между точками {distantion}");
---
+++ HW.Seminar3/Example23/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число чтобы получить таблицу кубов от единицы до него");
+double num = double.Parse(Console.ReadLine());
+for(double i = 1; i<=num; i++) Console.WriteLine($"{Math.Pow((i),3)}");
+if(num <= 0) Console.WriteLine("Неподходящее число");
